22470ec5865af201f0ab781eaa74451dd1a115fb
joshparkerj <superfakek@gmail.com>
2018-12-21 01:01:03 -0700

    complete the second tut page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="app">
-    <h1>Helloooooo Woooooorld</h1>
+  <div class="app full-width center-content">
+    <hello-component name="World" />
+    <h1>Hello World (Jesus? Is that you?)</h1>
+    <h2>black comedy amirite lmao</h2>
+    <h3>I'm cooler than a ...</h3>
+    <p>i ain't sayin nuffin</p>
+    <h5>I love you so much</h5>
   </div>
 </template>
-<!-- This bit doesn't seem to work yet, but imma keep on keepin on
+
 <script>
+import HelloComponent from './components/HelloComponent.vue'
+
 export default {
-  name: 'App'
+  components: {
+    HelloComponent
+  }
 }
 </script>
 
 <style scoped>
-h1 {
+.app {
+  background-color: beige;
+  border: solid black;
+  padding: 1%;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+.app h1 {
   color: blue;
+  font-family: 'Comic Sans MS';
+  display: block;
+  width: 50%;
+  margin: auto;
+  border-bottom: dashed orange;
+  padding: 5%;
+}
+p {
+  font-size: 8px;
+  margin-left: 10%;
 }
 </style>
--->
+
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>Hello {{ name }}!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.hello h1 {
+  color: red;
+}
+</style>
