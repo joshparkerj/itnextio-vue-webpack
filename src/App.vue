2fd5487431f369f7addd962c118282e564b24dbd
joshparkerj <superfakek@gmail.com>
2018-12-21 00:07:02 -0700

    complete part 1 of the delicious tut

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <h1>Helloooooo Woooooorld</h1>
+  </div>
+</template>
+<!-- This bit doesn't seem to work yet, but imma keep on keepin on
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
+-->
